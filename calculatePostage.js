1d4736937b3addffdb989bbfdde37314c5c1d319
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-03-07 14:20:50 -0700

    postage assignment minimum requirements

---
+++ calculatePostage.js
@@ -0,0 +1,58 @@
+module.exports = calculate;
+
+function calculate(req, res) {
+  console.log("Calculating rate.");
+  var weight = Number(req.query.weight).toFixed(2);
+  var type = req.query.type;
+  var results = Number(getResults(weight, type)).toFixed(2);
+  var typeSpelledOut = {
+    stamped : "Letters (Stamped)",
+    metered : "Letters (Metered)",
+    large : "Large Envelopes (Flats)",
+    retail : "First-Class Package Service—Retail"
+  };
+  params = {weight: weight, type: typeSpelledOut[type], results: results};
+  res.render('pages/results', params);
+}
+
+function getResults(weight, type) {
+  if ((type == 'stamped') && (weight <= 3.5)) {
+    if (weight <= 1) {
+      return .55;
+    } else if (weight <= 2) {
+      return .7;
+    } else if (weight <= 3) {
+      return .85;
+    } else {
+      return 1;
+    }
+  }
+  if ((type =='metered') && (weight <= 3.5)) {
+    if (weight <= 1) {
+      return .5;
+    } else if (weight <= 2) {
+      return .65;
+    } else if (weight <= 3) {
+      return .8;
+    } else {
+      return .95;
+    }
+  }
+  if ((type == 'large') && (weight <= 13)) {
+    return 1 + ((Math.ceil(weight) - 1) * .2)
+    //EX: 1 + ((ceiling(.3) - 1) * 2) = $1.00
+  }
+  if ((type == 'retail') && (weight <= 13)) {
+    //Assuming it is in zone 1 & 2
+    if (weight <= 4) {
+      return 3.8;
+    } else if (weight <= 8) {
+      return 4.6;
+    } else if (weight <= 12) {
+      return 5.3;
+    } else {
+      return 5.9;
+    }
+  }
+  return null; //should never come here.
+}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const calculatePostage = require('./calculatePostage.js');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', (req, res) => res.sendFile(__dirname + '/public/form.html'))
+  .get('/calculateResults', calculatePostage)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+

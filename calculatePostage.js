faf48975c9d08b7420e78a390f390a3609c5e6cb
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-03-07 19:03:44 -0700

    added extras for assignment

--- calculatePostage.js
+++ calculatePostage.js
@@ -4,19 +4,19 @@ function calculate(req, res) {
   console.log("Calculating rate.");
   var weight = Number(req.query.weight).toFixed(2);
   var type = req.query.type;
-  var results = Number(getResults(weight, type)).toFixed(2);
+  var zone = Number(req.query.zone);
+  var results = Number(getResults(weight, type, zone)).toFixed(2);
   var typeSpelledOut = {
-    stamped : "Letters (Stamped)",
-    metered : "Letters (Metered)",
-    large : "Large Envelopes (Flats)",
-    retail : "First-Class Package Service—Retail"
+    stamped: "Letters (Stamped)",
+    metered: "Letters (Metered)",
+    large: "Large Envelopes (Flats)",
+    retail: "First-Class Package Service—Retail"
   };
-  var zone = 2;
-  params = {weight: weight, type: typeSpelledOut[type], results: results, zone: zone};
+  params = { weight: weight, type: typeSpelledOut[type], results: results, zone: zone };
   res.render('pages/results', params);
 }
 
-function getResults(weight, type) {
+function getResults(weight, type, zone) {
   if ((type == 'stamped') && (weight <= 3.5)) {
     if (weight <= 1) {
       return .55;
@@ -28,7 +28,7 @@ function getResults(weight, type) {
       return 1;
     }
   }
-  if ((type =='metered') && (weight <= 3.5)) {
+  if ((type == 'metered') && (weight <= 3.5)) {
     if (weight <= 1) {
       return .5;
     } else if (weight <= 2) {
@@ -43,17 +43,145 @@ function getResults(weight, type) {
     return 1 + ((Math.ceil(weight) - 1) * .2)
     //EX: 1 + ((ceiling(.3) - 1) * 2) = $1.00
   }
+  //lookup table for price for [zone][weight]
+  price = [[
+    3.8,
+    3.8,
+    3.8,
+    3.8,
+    4.6,
+    4.6,
+    4.6,
+    4.6,
+    5.3,
+    5.3,
+    5.3,
+    5.3,
+    5.9
+  ],
+  [
+    3.8,
+    3.8,
+    3.8,
+    3.8,
+    4.6,
+    4.6,
+    4.6,
+    4.6,
+    5.3,
+    5.3,
+    5.3,
+    5.3,
+    5.9
+  ],
+  [
+    3.85,
+    3.85,
+    3.85,
+    3.85,
+    4.65,
+    4.65,
+    4.65,
+    4.65,
+    5.35,
+    5.35,
+    5.35,
+    5.35,
+    5.95
+  ],
+  [
+    3.9,
+    3.9,
+    3.9,
+    3.9,
+    4.7,
+    4.7,
+    4.7,
+    4.7,
+    5.4,
+    5.4,
+    5.4,
+    5.4,
+    6.05
+  ],
+  [
+    3.95,
+    3.95,
+    3.95,
+    3.95,
+    4.75,
+    4.75,
+    4.75,
+    4.75,
+    5.45,
+    5.45,
+    5.45,
+    5.45,
+    6.15
+  ],
+  [
+    4,
+    4,
+    4,
+    4,
+    4.8,
+    4.8,
+    4.8,
+    4.8,
+    5.5,
+    5.5,
+    5.5,
+    5.5,
+    6.2
+  ], 
+  [
+    4.05, 
+    4.05,
+    4.05,
+    4.05,
+    4.9,
+    4.9,
+    4.9,
+    4.9,
+    5.65,
+    5.65,
+    5.65,
+    5.65,
+    6.4
+  ],
+  [
+    4.2,
+    4.2,
+    4.2,
+    4.2,
+    5,
+    5,
+    5,
+    5,
+    5.75,
+    5.75,
+    5.75,
+    5.75,
+    6.5
+  ],
+  [
+    4.2,
+    4.2,
+    4.2,
+    4.2,
+    5,
+    5,
+    5,
+    5,
+    5.75,
+    5.75,
+    5.75,
+    5.75,
+    6.5
+  ]];
+
   if ((type == 'retail') && (weight <= 13)) {
-    //Assuming it is in zone 1 & 2
-    if (weight <= 4) {
-      return 3.8;
-    } else if (weight <= 8) {
-      return 4.6;
-    } else if (weight <= 12) {
-      return 5.3;
-    } else {
-      return 5.9;
-    }
+    return price[zone - 1][Math.ceil(weight) - 1];
   }
   return null; //should never come here.
 }
